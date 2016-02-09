761d28ca63e93bdb55edc6a9937044c062699768
Victor <victor.miletskiy@gmail.com>
2016-02-09 16:57:52 +0200

    Docs update

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -5,48 +5,48 @@ NG_DOCS={
   "pages": [
     {
       "section": "api",
-      "id": "govright.llServices",
-      "shortName": "govright.llServices",
+      "id": "govright.platformServices",
+      "shortName": "govright.platformServices",
       "type": "overview",
-      "moduleName": "govright.llServices",
-      "shortDescription": "The govright.llServices module provides services that encapsulate",
-      "keywords": "add angular angularjs api common corpus corpusservices dependencies dependency dist encapsulate example gettext govright html include interacting js ll llservices module myapp ng-app ngmaterial org overview rocketeer router script services src techniques ui var"
+      "moduleName": "govright.platformServices",
+      "shortDescription": "The govright.platformServices module provides services that encapsulate",
+      "keywords": "add angular angularjs api common corpus corpusservices dependencies dependency dist encapsulate example gettext govright html include interacting js module myapp ng-app ngmaterial org overview platform platformservices rocketeer router script services src techniques ui var"
     },
     {
       "section": "api",
-      "id": "govright.llServices.llAuth",
-      "shortName": "govright.llServices.llAuth",
+      "id": "govright.platformServices.grAuth",
+      "shortName": "govright.platformServices.grAuth",
       "type": "object",
-      "moduleName": "govright.llServices",
+      "moduleName": "govright.platformServices",
       "shortDescription": "Login helper.",
-      "keywords": "$location $on $q $rootscope $scope $state $window angular api app arg authurl automatically block broadcast broadcasted builds cached called case catch check checklogin config console controller corpus creates credentials current currentuser data default email error event examples expired facebook function govright handles hasn helper host initialised initsocialhandler instance json lb level llauth llfacebook llservices log logged login logout loopback loopbackauth message method module null object pass password payload populated populates popup previous process processauthmessage promise property resolved response restore result saves session sessions setcurrentuser site siteconfig snippet social sociallogin starts stuff successful test top typically undefined url user username users var warn window"
+      "keywords": "$location $on $q $rootscope $scope $state $window angular api app arg authurl automatically block broadcast broadcasted builds cached called case catch check checklogin console controller corpus creates credentials current currentuser data default email error event examples expired facebook function govright grauth grfacebook handles hasn helper host http initialised initsocialhandler instance lb level log logged login logout loopback loopbackauth message method module null object org pass password payload platformservices populated populates popup previous process processauthmessage promise property resolved response restore result saves session sessions setcurrentuser site snippet social sociallogin starts stuff successful test top typically undefined url user username users var warn window"
     },
     {
       "section": "api",
-      "id": "govright.llServices.llFacebook",
-      "shortName": "govright.llServices.llFacebook",
+      "id": "govright.platformServices.grFacebook",
+      "shortName": "govright.platformServices.grFacebook",
       "type": "object",
-      "moduleName": "govright.llServices",
+      "moduleName": "govright.platformServices",
       "shortDescription": "Facebook auth/posting helper.",
-      "keywords": "$q $window access accesstoken action api app appid behalf clearstorage config current data delete facebook facebookdata fb future getaccesstoken getappid getnamespace govright helper init initialise initialization llfacebook llservices loadaccessdata method namespace null object post postaction posting props remember result save saveaccessdata saves saving sessions storage store stored token true user"
+      "keywords": "$q $window access accesstoken action api app appid behalf clearstorage config current data delete facebook facebookdata fb future getaccesstoken getappid getnamespace govright grfacebook helper init initialise initialization loadaccessdata method namespace null object platformservices post postaction posting props remember result save saveaccessdata saves saving sessions storage store stored token true user"
     },
     {
       "section": "api",
-      "id": "govright.llServices.llLocale",
-      "shortName": "govright.llServices.llLocale",
+      "id": "govright.platformServices.grLocale",
+      "shortName": "govright.platformServices.grLocale",
       "type": "object",
-      "moduleName": "govright.llServices",
+      "moduleName": "govright.platformServices",
       "shortDescription": "Locale helper.",
-      "keywords": "$rootscope angular api app application apply arguments array check checked code codes controller current defaullocale default defaultlocale detect determinelocalecode dir direction discussion en english example examples extended extendedlookup extract extracted false function getstring gettext gettextcatalog govright helper instance isvalid key law list lllocale llservices locale localecode localedir locales localised lookups lookupstring ltr method module null object property returned set setcurrent setdefault setlocales startup string target text title translation true typically valid"
+      "keywords": "$rootscope angular api app application apply arguments array check checked code codes controller current defaullocale default defaultlocale detect determinelocalecode dir direction discussion en english example examples extended extendedlookup extract extracted false function getstring gettext gettextcatalog govright grlocale helper instance isvalid key law list locale localecode localedir locales localised lookups lookupstring ltr method module null object platformservices property returned set setcurrent setdefault setlocales startup string target text title translation true typically valid"
     },
     {
       "section": "api",
-      "id": "govright.llServices.llMessage",
-      "shortName": "govright.llServices.llMessage",
+      "id": "govright.platformServices.grMessage",
+      "shortName": "govright.platformServices.grMessage",
       "type": "object",
-      "moduleName": "govright.llServices",
+      "moduleName": "govright.platformServices",
       "shortDescription": "Various site messages, like 404, dialog popups, success toast, etc.",
-      "keywords": "$mddialog $mdtoast $state $stateprovider api button cancel canceled catch changing clicked close complete confirm content controller custom dialog display error error404 example govright html llmessage llservices message messages method ngmaterial object occurred params popups request requires route router separate set site staticpagecontroller success templateurl text title toast transition transitions ui undefined url user"
+      "keywords": "$mddialog $mdtoast $state $stateprovider api button cancel canceled catch changing clicked close complete confirm content controller custom dialog display error error404 example govright grmessage html message messages method ngmaterial object occurred params platformservices popups request requires route router separate set site staticpagecontroller success templateurl text title toast transition transitions ui undefined url user"
     }
   ],
   "apis": {
@@ -54,7 +54,7 @@ NG_DOCS={
   },
   "__file": "_FAKE_DEST_/js/docs-setup.js",
   "__options": {
-    "startPage": "/api/govright.llServices",
+    "startPage": "/api/govright.platformServices",
     "scripts": [
       "js/angular.min.js",
       "js/angular-animate.min.js",
@@ -63,11 +63,11 @@ NG_DOCS={
     "styles": [
       "css/bower_components/gulp-ngdocs-supplemental/dist/style.css"
     ],
-    "title": "GovRight Legislation Lab Services",
+    "title": "GovRight Platform Services",
     "html5Mode": false,
     "editExample": true,
     "navTemplate": "./ngdocs_assets/navbar.html",
-    "navContent": "<ul class=\"nav nav-custom pull-right\">\n  <li>\n    <a href=\"https://github.com/GovRight/legislation-lab-services\" target=\"_blank\">\n      <i class=\"icon-github icon-white\"></i> GitHub\n    </a>\n  </li>\n</ul>\n",
+    "navContent": "<ul class=\"nav nav-custom pull-right\">\n  <li>\n    <a href=\"https://github.com/GovRight/platform-services\" target=\"_blank\">\n      <i class=\"icon-github icon-white\"></i> GitHub\n    </a>\n  </li>\n</ul>\n",
     "navTemplateData": {},
     "loadDefaults": {
       "angular": true,
@@ -77,7 +77,7 @@ NG_DOCS={
   },
   "html5Mode": false,
   "editExample": true,
-  "startPage": "/api/govright.llServices",
+  "startPage": "/api/govright.platformServices",
   "scripts": [
     "js/angular.min.js",
     "js/angular-animate.min.js",
